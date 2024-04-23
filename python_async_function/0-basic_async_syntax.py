1ec4081c9307c3d13f33735c9492c14650c27f0a
Echezona Oji <echyth@yahoo.com>
2024-04-24 09:46:33 +1000

    fix(task 0): change param type for max_delay to int

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: float = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """asynchronous coroutine that takes in an integer argument
     (max_delay, with a default value of 10) named wait_random that
     waits for a random delay between 0 and max_delay
