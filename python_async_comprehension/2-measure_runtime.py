b7f52f59de7dbb18e6b4151a7a4b373bb05be1ae
Echezona Oji <echyth@yahoo.com>
2024-04-24 12:15:56 +1000

    fix(task 2): break long import line into two lines

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -3,7 +3,8 @@
 import asyncio
 import time
 
-async_comprehension = __import__("1-async_comprehension").async_comprehension
+async_comprehension = __import__(
+    "1-async_comprehension").async_comprehension
 
 
 async def measure_runtime() -> float:
