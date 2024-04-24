2d23292699a1f2ef1c8586c9ee073e3670dcf2e8
Echezona Oji <echyth@yahoo.com>
2024-04-24 12:09:03 +1000

    feat(task 2): initial commit

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module for measure_runtime coroutine"""
+import asyncio
+import time
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime() -> float:
+    """execute async_comprehension four times in parallel
+    using asyncio.gather and returns the total runtime.
+    """
+    s = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    print()
+    elapsed_time = time.perf_counter() - s
+    return elapsed_time
