abb284d7e531d5bc496f08e51d1a71decd93df79
Echezona Oji <echyth@yahoo.com>
2024-05-01 00:31:42 +1000

    fix(task 12): change nginx_collection to collection

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -8,17 +8,17 @@ import pymongo
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    ngnix_collection: pymongo.collection.Collection \
+    collection: pymongo.collection.Collection \
         = client.logs.nginx
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print(f"{ngnix_collection.count_documents({})} logs")
+    print(f"{collection.count_documents({})} logs")
     print("Methods:")
 
     for method in methods:
-        method_count = ngnix_collection.count_documents({"method": method})
+        method_count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {method_count}")
 
-    status_req_count = ngnix_collection.count_documents({
+    status_req_count = collection.count_documents({
         "method": "GET",
         "path": "/status"
     })
