057f862f3d0ea96150ac36213d90fa226b1cc149
Echezona Oji <echyth@yahoo.com>
2024-05-01 00:26:45 +1000

    fix(task 12): modify code to run when imported

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -6,23 +6,22 @@ from pymongo import MongoClient
 import pymongo
 
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    ngnix_collection: pymongo.collection.Collection \
-        = client.logs.nginx
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print(f"{ngnix_collection.count_documents({})} logs")
-    print("Methods:")
+client = MongoClient('mongodb://127.0.0.1:27017')
+ngnix_collection: pymongo.collection.Collection \
+    = client.logs.nginx
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+print(f"{ngnix_collection.count_documents({})} logs")
+print("Methods:")
 
-    for method in methods:
-        method_count = ngnix_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {method_count}")
+for method in methods:
+    method_count = ngnix_collection.count_documents({"method": method})
+    print(f"\tmethod {method}: {method_count}")
 
-    status_req_count = ngnix_collection.count_documents({
-        "method": "GET",
-        "path": "/status"
-    })
+status_req_count = ngnix_collection.count_documents({
+    "method": "GET",
+    "path": "/status"
+})
 
-    print(f"{status_req_count} status check")
+print(f"{status_req_count} status check")
 
-    client.close()
+client.close()
