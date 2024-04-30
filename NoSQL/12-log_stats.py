800c7b5c1885a2801b6b433e28fde8a91b4f852b
Echezona Oji <echyth@yahoo.com>
2024-05-01 00:29:21 +1000

    fix(task 12): revert to run when invoked directly

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -6,22 +6,21 @@ from pymongo import MongoClient
 import pymongo
 
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-ngnix_collection: pymongo.collection.Collection \
-    = client.logs.nginx
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-print(f"{ngnix_collection.count_documents({})} logs")
-print("Methods:")
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    ngnix_collection: pymongo.collection.Collection \
+        = client.logs.nginx
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print(f"{ngnix_collection.count_documents({})} logs")
+    print("Methods:")
 
-for method in methods:
-    method_count = ngnix_collection.count_documents({"method": method})
-    print(f"\tmethod {method}: {method_count}")
+    for method in methods:
+        method_count = ngnix_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
 
-status_req_count = ngnix_collection.count_documents({
-    "method": "GET",
-    "path": "/status"
-})
+    status_req_count = ngnix_collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+    })
 
-print(f"{status_req_count} status check")
-
-client.close()
+    print(f"{status_req_count} status check")
