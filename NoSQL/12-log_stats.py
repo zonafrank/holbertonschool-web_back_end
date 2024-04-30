b6910ed2ce9132e2f23507e31ddfeb892545d332
Echezona Oji <echyth@yahoo.com>
2024-05-01 00:35:23 +1000

    feat(task 12): troubleshooting why code does not print in checker

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
-"""This module provides some stats about Nginx logs stored in MongoDB"""
+"""Module that provides some stats about Nginx logs
+stored in MongoDB
+"""
 from pymongo import MongoClient
+import pymongo
 
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
-    total_count = collection.count_documents({})
-    print('{} logs'.format(total_count))
-
-    print('Methods:')
+    collection: pymongo.collection.Collection \
+        = client.logs.nginx
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        method_count = collection.count_documents({"method" : method})
-        print('\tmethod {}: {}'.format(method, method_count))
+    print(f"{collection.count_documents({})} logs")
+    print("Methods:")
 
-    status_count = collection.count_documents({'path': "/status"})
-    print('{} status check'.format(status_count))
+    for method in methods:
+        method_count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
+
+    status_req_count = collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+    })
+
+    print(f"{status_req_count} status check")
