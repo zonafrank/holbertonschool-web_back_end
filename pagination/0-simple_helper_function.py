921287b29982ef73d34badb5f12e0558a95885fc
Echezona Oji <echyth@yahoo.com>
2024-05-01 09:04:52 +1000

    feat(task 0): initial function implementation

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""Module for index_range function"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """When given page and pagesize as arguments Calculates the
+      start and end index"""
+    start_index: int = (page - 1) * page_size
+    end_index: int = start_index + page_size
+    return (page, end_index)
