860e1e01003eb1fbf1be068f372f42c67ede087b
Echezona Oji <echyth@yahoo.com>
2024-05-03 00:01:47 +1000

    feat (task 3): initial commit

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,35 @@
+export default class Currency {
+  constructor(code, name) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(val) {
+    if (!(typeof (val) === 'string')) {
+      // eslint-disable-next-line no-throw-literal
+      throw 'code must be a string';
+    }
+    this._code = val;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (!(typeof (val) === 'string')) {
+      // eslint-disable-next-line no-throw-literal
+      throw 'name must be a string';
+    }
+
+    this._name = val;
+  }
+
+  displayFullCurrency() {
+    console.log(`${this._name} (${this._code})`);
+  }
+}
