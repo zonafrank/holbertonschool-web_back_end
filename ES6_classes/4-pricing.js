d2bdff002a445aedfea81a1409b35c4e2f968770
Echezona Oji <echyth@yahoo.com>
2024-05-03 00:45:36 +1000

    feat (task 4): initial commit

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(val) {
+    if (typeof (val) !== 'number') {
+      // eslint-disable-next-line no-throw-literal
+      throw 'amount must be a number';
+    }
+    this._amount = val;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(val) {
+    if (!(val instanceof Currency)) {
+      // eslint-disable-next-line no-throw-literal
+      throw 'currency must be of type Currency';
+    }
+    this._currency = val;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+}
