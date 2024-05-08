fa6c18a016e2d44a739ddb8e49b70595842cb529
Echezona Oji <echyth@yahoo.com>
2024-05-09 09:53:12 +1000

    feat(task 2): initial commit

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
