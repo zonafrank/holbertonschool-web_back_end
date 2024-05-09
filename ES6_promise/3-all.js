e358a0ec2f5a7a57e22938aac769f4e836418702
Echezona Oji <echyth@yahoo.com>
2024-05-09 15:09:29 +1000

    fix (task 3): troubleshoot failing test

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((res) => {
       console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .catch((err) => {
-      console.log("Signup system offline");
+    .catch(() => {
+      console.log('Signup system offline');
     });
 }
