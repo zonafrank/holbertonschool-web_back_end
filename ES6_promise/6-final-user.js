e51c792e868bad444af6aca456b99880e4d29b72
Echezona Oji <echyth@yahoo.com>
2024-05-09 13:29:21 +1000

    fix(task 6): troubleshooting lint error

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,27 +1,25 @@
-#!/usr/bin/node
-
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName)
     .then((res) => ({
-      status: 'fulfilled',
-      value: res,
+      status: "fulfilled",
+      value: res
     }))
     .catch((err) => ({
-      status: 'rejected',
-      value: err.toString(),
+      status: "rejected",
+      value: err.toString()
     }));
 
   const photoPromise = uploadPhoto(fileName)
     .then((res) => ({
-      status: 'fulfilled',
-      value: res,
+      status: "fulfilled",
+      value: res
     }))
     .catch((err) => ({
-      status: 'rejected',
-      value: err.toString(),
+      status: "rejected",
+      value: err.toString()
     }));
 
   return Promise.all([userPromise, photoPromise]);
