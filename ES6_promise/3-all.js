a3e9f3b542d744c4981c38a4a9b36eb3edb9e4f5
Echezona Oji <echyth@yahoo.com>
2024-05-09 11:10:02 +1000

    feat (task 3): initial commit

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    });
+}
