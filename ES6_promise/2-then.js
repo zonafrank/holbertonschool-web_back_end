6b8ac38a9c38fba88e2ba62b9ba51a5b06c8af4b
Echezona Oji <echyth@yahoo.com>
2024-05-09 14:44:42 +1000

    feat (task 2): remove shebang line

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,10 +1,8 @@
-#!/usr/bin/node
-
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => ({ status: 200, body: 'success' }))
+  return promise
+    .then(() => ({ status: 200, body: "success" }))
     .catch(() => new Error())
     .finally(() => {
-      console.log('Got a response from the API');
+      console.log("Got a response from the API");
     });
 }
