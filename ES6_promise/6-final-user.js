72a40e9f72f065e4db77db419d43fdf0344e68be
Echezona Oji <echyth@yahoo.com>
2024-05-09 12:44:36 +1000

    feat(task 6): initial commit

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,26 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName)
+    .then((res) => ({
+      status: 'fulfilled',
+      value: res,
+    }))
+    .catch((err) => ({
+      status: 'rejected',
+      value: err.toString(),
+    }));
+
+  const photoPromise = uploadPhoto(fileName)
+    .then((res) => ({
+      status: 'fulfilled',
+      value: res,
+    }))
+    .catch((err) => ({
+      status: 'rejected',
+      value: err.toString(),
+    }));
+
+  return Promise.all([userPromise, photoPromise]);
+}
