61f812949df98a77625412a0c99f58ecead65cca
Echezona Oji <echyth@yahoo.com>
2024-05-14 14:23:01 +0800

    feat (task 8): initial commit

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet (set, startString) {
+  let result = '';
+  if (startString === '') return result;
+
+  for (const item of set) {
+    if (item.startsWith(startString)) {
+      if (result !== '') {
+        result += '-';
+      }
+      result += item.slice(startString.length);
+    }
+  }
+  return result;
+}
