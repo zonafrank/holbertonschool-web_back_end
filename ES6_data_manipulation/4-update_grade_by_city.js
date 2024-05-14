2937f25962bb0139cf6957d67c1ccd2f462f077d
Echezona Oji <echyth@yahoo.com>
2024-05-14 12:00:56 +0800

    feat (task 4): initial commit

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+export default function updateStudentGradeByCity (studentList, city, newGrades) {
+  return studentList
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const found = newGrades.find((item) => item.studentId === student.id);
+      if (found) {
+        return { ...student, grade: found.grade };
+      } else {
+        return { ...student, grade: 'N/A' };
+      }
+    });
+}
