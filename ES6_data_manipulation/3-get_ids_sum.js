faf2b57a75802a96352a59da50738857e41ec507
Echezona Oji <echyth@yahoo.com>
2024-05-14 15:26:16 +0800

    fix (task 3): fix eslint error

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,3 @@
 export default function getStudentIdsSum(studentObjArray) {
-  return studentObjArray.reduce((acc, item) => {
-    return acc + item.id;
-  }, 0);
+  return studentObjArray.reduce((acc, item) => acc + item.id, 0);
 }
