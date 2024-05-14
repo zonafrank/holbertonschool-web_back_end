9a1839fc30ebf9d63f02d83bb74818fb55a70f43
Echezona Oji <echyth@yahoo.com>
2024-05-14 14:41:29 +0800

    feat (task 10): initial commit

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,12 @@
+export default function updateUniqueItems (mapObject) {
+  if (!(mapObject instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const item of mapObject) {
+    const [k, v] = item;
+    if (v === 1) {
+      mapObject.set(k, 100);
+    }
+  }
+}
